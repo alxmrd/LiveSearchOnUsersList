ab4e2ec832d77a122b84fb58b6cd97c2b52aa4bf
Alexandros Mironidis <alex.m@novoville.ocm>
2020-04-06 17:17:12 +0300

    apply styles on list of user cards

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -16,11 +16,22 @@ import Divider from "@material-ui/core/Divider";
 import { withStyles } from "@material-ui/core";
 import { connect } from "react-redux";
 import { expandCard } from "../store/actions/actions";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import "./loader.css";
-
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#62BFEC"
+    }
+  }
+});
 const styles = theme => ({
   root: {
-    width: "100%"
+    width: "100%",
+    background: "#ffffff",
+    "&:hover": {
+      background: "#fafafa"
+    }
   },
 
   media: {
@@ -45,6 +56,9 @@ const styles = theme => ({
   text: {
     marginLeft: "10px",
     marginRight: "10px"
+  },
+  cardHeader: {
+    background: "#ffffff"
   }
 });
 
@@ -75,26 +89,30 @@ class UserCard extends React.Component {
 
                 <Collapse in={user.id === id} timeout="auto" unmountOnExit>
                   <Divider />
-                  <CardHeader
-                    avatar={
-                      <Avatar
-                        aria-label={user.name}
-                        className={classes.avatar}
-                        src={userIcon}
-                      />
-                    }
-                    title={user.email}
-                    subheader={
-                      <div>
+                  <MuiThemeProvider theme={theme}>
+                    <CardHeader
+                      className={classes.cardHeader}
+                      avatar={
+                        <Avatar
+                          aria-label={user.name}
+                          className={classes.avatar}
+                          src={userIcon}
+                        />
+                      }
+                      title={user.email}
+                      titleTypographyProps={{ color: "primary", variant: "h6" }}
+                      subheader={
                         <div>
-                          {user.address.street + ", " + user.address.suite}{" "}
+                          <div>
+                            {user.address.street + ", " + user.address.suite}{" "}
+                          </div>
+                          <div>
+                            {user.address.zipcode + ", " + user.address.city}{" "}
+                          </div>
                         </div>
-                        <div>
-                          {user.address.zipcode + ", " + user.address.city}{" "}
-                        </div>
-                      </div>
-                    }
-                  />
+                      }
+                    />
+                  </MuiThemeProvider>
                 </Collapse>
               </Card>
             </ListItem>
